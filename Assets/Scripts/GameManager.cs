5e459629011c50d3735d98b592b33467ab800c86
HDonovan96 <harry.bd11@gmail.com>
2020-03-03 10:51:39 +0000

    Code SetUps for camera and renderscreen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,23 @@
 
 public class GameManager : MonoBehaviour
 {
+    public float renderScreenScaleModifier = 0.9f;
+    private void SetUpCamera(Camera camera, int index, RenderTexture renderTexture = null)
+    {
+        camera.depth = -index;
+        camera.cullingMask = index;
+        camera.targetTexture = renderTexture;
+    }
 
+    private void SetUpRenderScreen(GameObject renderScreen, int index, RenderTexture renderTexture = null)
+    {
+        // Z coordinate is 2 - index in order to get correct layering when rendering.
+        renderScreen.transform.position = new Vector3(0.0f, 0.0f, (2 - index));
+        renderScreen.transform.localScale *= renderScreenScaleModifier;
+
+        if (renderTexture != null)
+        {
+            renderScreen.GetComponent<Material>().mainTexture = renderTexture;
+        }
+    }
 }
