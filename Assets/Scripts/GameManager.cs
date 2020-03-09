a6f777002038437e5e00697f6d8c1f67c7d2a701
HDonovan96 <harry.bd11@gmail.com>
2020-03-09 11:32:52 +0000

    Marked GameManager script as .old. Will be attempting to do it without using render textures

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float renderScreenScaleModifier = 0.9f;
-    private void SetUpCamera(Camera camera, int index, RenderTexture renderTexture = null)
+    // Start is called before the first frame update
+    void Start()
     {
-        camera.depth = -index;
-        camera.cullingMask = index;
-        camera.targetTexture = renderTexture;
+        
     }
 
-    private void SetUpRenderScreen(GameObject renderScreen, int index, RenderTexture renderTexture = null)
+    // Update is called once per frame
+    void Update()
     {
-        // Z coordinate is 2 - index in order to get correct layering when rendering.
-        renderScreen.transform.position = new Vector3(0.0f, 0.0f, (2 - index));
-        renderScreen.transform.localScale *= renderScreenScaleModifier;
-
-        if (renderTexture != null)
-        {
-            renderScreen.GetComponent<Material>().mainTexture = renderTexture;
-        }
+        
     }
 }
